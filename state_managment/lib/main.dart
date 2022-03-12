d526bf44cdc1d809219c5a84399c3209dd0fdd82
gasimzade13 <elmir.gasimzade@mail.ru>
2022-03-12 21:35:31 +0400

    State managment

---
+++ state_managment/lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:state_managment/counter.dart';
+class MyAppp extends StatelessWidget {
+  const MyAppp({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Sayac Ana Sehife"),),
+      body: Container(
+        child: Center(
+          
+        ),
+      ),
+    );
+  }
+}
+
